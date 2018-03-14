eb5d47927119b242cbed97ee5f8932b696cf72e9
廖慶麟 <hohey670@gmail.com>
2018-03-14 17:58:26 +0800

    implement Input, Output, Pedal view components conforms to different protocol, all of then subclass to SigPathComponent

---
+++ PositiveGrid-iOS-Assignment/Output.swift
@@ -0,0 +1,29 @@
+//
+//  Output.swift
+//  PositiveGrid-iOS-Assignment
+//
+//  Created by 廖慶麟 on 2018/3/14.
+//  Copyright © 2018年 廖慶麟. All rights reserved.
+//
+
+import Foundation
+import Bond
+import ReactiveKit
+
+internal final class Output: SigPathComponent {
+    
+    // MARK: - Init
+    public convenience init() {
+        self.init(frame: CGRect.zero)
+    
+        self.type = .output
+    }
+    
+    private override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
